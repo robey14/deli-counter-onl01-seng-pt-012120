8fe82ab4804e7945e7f192a6bba3798d0197740c
John Michael Robey <robey14@gmail.com>
2020-01-29 22:29:56 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def line(array)
+  if array.length == 0 
+  puts "The line is currently empty."
+  elsif array. length > 0 
+  names_list=array.map.with_index do |name,index|
+    "#{index}.#{name}"
+    end
+     puts "The line is currently #{names_list}"
+  end
+end
+
+def take_a_number(katz_deli,name)
+
+
+
+####def oxford_comma(array)
+  #if array.length == 2
+   # return "#{array[0]} and #{array[1]}"
+  #elsif array.length > 2
+   # array[-1].insert(0 , "and ")
+ # end
+  #array.join(", ")    
+#end
