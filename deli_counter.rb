5128ac187300c2e976dca18c468ba81877f0fe57
John Michael Robey <robey14@gmail.com>
2020-01-30 01:08:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ end
 
 def take_a_number(array,name)
   array << name
-  puts "Welcome,  #{name}. You are numer #{array.index(name) +1} in line."
+  puts "Welcome,#{name}. You are numer #{array.index(name) +1} in line."
 end
 
 def now_serving()
